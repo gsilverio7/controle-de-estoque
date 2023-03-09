b0610f820a883031650770cb2952b892b5203d8e
Gabriel Silvério <bielsil27@gmail.com>
2023-03-09 13:12:37 -0300

    criar gráfico de fluxo de caixa
    adicionar gráfico de fluxo de caixa ao dashboard. Por enquanto meramente ilustrativo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,10 @@
 @stop
 
 @section('content')
-    <p>Welcome to this beautiful admin panel.</p>
+    <div style="width: 500px; height: 700px;">
+        <h4 class="text-center">Controle de Caixa</h4>
+        <canvas id="myChart"></canvas>
+    </div>
 @stop
 
 @section('css')
@@ -15,5 +18,44 @@
 @stop
 
 @section('js')
-    <script> console.log('Hi!'); </script>
+    <script> 
+        const ctx = document.getElementById('myChart');
+        const data = {
+            labels: ['jan','fev','mar','abr','mai','jun'],
+            datasets: [
+                {
+                    label: 'Dataset 1',
+                    data: [100,200,-30,401,575,686],
+                    backgroundColor: ['#00a65a','#00a65a','#f56954','#00a65a','#00a65a','#00a65a'],
+                }
+            ]
+        };
+
+        window.myObjBar = new Chart(ctx, {
+            type: 'bar',
+            data: data,
+            options: {
+                responsive: true,
+                plugins: {
+                    legend: {
+                        position: 'top',
+                    },
+                    title: {
+                        display: true,
+                        text: 'Chart.js Bar Chart'
+                    }
+                },
+                legend: {
+                    display: false
+                },
+                tooltips: {
+                    callbacks: {
+                    label: function(tooltipItem) {
+                            return tooltipItem.yLabel;
+                        }
+                    }
+                }
+            },
+        }); 
+    </script>
 @stop
